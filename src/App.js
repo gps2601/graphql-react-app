9d8277cb523f3e6e013d61fb26475c1da472a4c0
Geoffrey Smith <smith260194@gmail.com>
2019-11-06 22:35:14 +0000

    Adding form to add new users and making the page update using refetchQueries

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { Query, ApolloConsumer } from "react-apollo";
+import { Query, ApolloConsumer, Mutation } from "react-apollo";
 import { gql } from "apollo-boost";
 
 import Person from "./Person";
+import PersonForm from "./PersonForm";
 
 const ALL_PERSONS = gql`
   {
@@ -14,15 +15,46 @@ const ALL_PERSONS = gql`
   }
 `;
 
+const CREATE_PERSON = gql`
+  mutation createPerson(
+    $name: String!
+    $street: String!
+    $city: String!
+    $phone: String
+  ) {
+    addPerson(name: $name, street: $street, city: $city, phone: $phone) {
+      name
+      phone
+      id
+      address {
+        street
+        city
+      }
+    }
+  }
+`;
+
 const App = () => {
   return (
-    <ApolloConsumer>
-      {client => (
-        <Query query={ALL_PERSONS}>
-          {result => <Person result={result} client={client} />}
-        </Query>
-      )}
-    </ApolloConsumer>
+    <div>
+      <ApolloConsumer>
+        {client => (
+          <Query 
+            query={ALL_PERSONS} 
+            // pollInterval={2000}
+          >
+            {result => <Person result={result} client={client} />}
+          </Query>
+        )}
+      </ApolloConsumer>
+      <h2>create new</h2>
+      <Mutation
+        mutation={CREATE_PERSON}
+        refetchQueries={[{ query: ALL_PERSONS }]}
+      >
+        {addPerson => <PersonForm addPerson={addPerson} />}
+      </Mutation>
+    </div>
   );
 };
 
